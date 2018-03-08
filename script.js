6029b52fa59fe61ab78d9264436b04c53dbadc44
Uros Mitrovic <urosmitrovic98@gmail.com>
2018-03-08 15:37:21 +0100

    Added map and checks for possible errors

--- script.js
+++ script.js
@@ -11,10 +11,38 @@ function locationUsageAlert (){
 function userLocation() {
     if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition((e) => {
-            console.log(e);
-        });
+            
+            // Show user location on a map
+            
+            const locationMap = document.querySelector(".locationMap");
+
+            let locat = e.coords.latitude + "," + e.coords.longitude;
+            let imgURL = "https://maps.googleapis.com/maps/api/staticmap?center=" + locat +
+            "&zoom=14&size=400x300&key=AIzaSyBu-916DdpKAjTmJNIgngS6HL_kDIKU0aU";
+
+            locationMap.src = imgURL;
+        }, locationError());
     }
     else {
         alert("This browser doesn't support geolocation!");
+    }
+}
+
+// Error collection
+
+function locationError(e) {
+    switch(e.code) {
+        case e.PERMISSION_DENIED:
+            alert("User denied the request for Geolocation.")
+            break;
+        case e.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable.")
+            break;
+        case e.TIMEOUT:
+            alert("The request to get user location timed out.")
+            break;
+        case e.UNKNOWN_ERROR:
+            alert("An unknown error occurred.")
+            break;
     }
 }
